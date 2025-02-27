d3f22c035f866506017725a82a0ed77982da92f3
Sasha Lorens <js.slorens@gmail.com>
2025-02-27 14:20:01 +0200

    Setting up Redux Toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,22 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import MainPage from './MainPage';
+
+import store from './shared/store/store.js';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <App />,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
+  </Provider>,
 );
